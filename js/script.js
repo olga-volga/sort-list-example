1bb03c24edcee1644be6b57fa2417dde2073d84b
olga <yaolmi@yandex.ru>
2021-07-02 14:44:46 +0700

    add js script for sorting

--- js/script.js
+++ js/script.js
@@ -2,5 +2,26 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
-	
+	const list = document.querySelector('.sort__list'),
+		  listItem = document.querySelectorAll('li'),
+		  btn = document.querySelector('.sort__btn');
+
+	function sortList() {
+		let cityList = [];
+
+		listItem.forEach(item => {
+			cityList.push(item.innerHTML);
+		});
+
+		cityList.sort();
+
+		list.innerHTML = "";
+
+		cityList.forEach(item => {
+			list.innerHTML += `<li>${item}</li>`;
+		});
+	}
+
+	btn.addEventListener('click', sortList);
+
 });
